ac7265061bae90bc971f0668c26f9fef9efd5e6d
Jorge Baez <jibm_82@yahoo.com.mx>
2022-05-23 15:59:10 -0500

    Update copys

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
+        Play Business
       </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
+      <p className="text-gray-500 text-lg mb-4">
+        Selecciona el monto que deseas invertir
       </p>
       <Checkout></Checkout>
     </div>
